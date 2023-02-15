bfb0f7d5f25b3c67d47c32d0381e8acb07d96d0e
qizhang <13681466643@163.com>
2023-02-15 23:50:52 +0800

    feat:cli test

--- src/node/cli.ts
+++ src/node/cli.ts
@@ -1,21 +1,31 @@
-import { Command } from "commander";
-const program = new Command();
+#!/usr/bin/env node
+import { program } from "commander";
+import inquirer from "inquirer";
+import ora from "ora"
+// 打印参数
+console.log(process.argv);
 
 program
-  .name("string-util")
-  .description("CLI to some JavaScript string utilities")
-  .version("0.8.0");
-
-program
-  .command("split")
-  .description("Split a string into substrings and display as an array")
-  .argument("<string>", "string to split")
-  .option("--first", "display just the first substring")
-  .option("-s, --separator <char>", "separator character", ",")
-  .action((str, options) => {
-    const limit = options.first ? 1 : undefined;
-    console.log(str.split(options.separator, limit));
+  .command("create <projectName>")
+  .description("create a new project")
+  .alias("c")
+  .option("-r, --react", "react template")
+  .option("-v, --vue", "vue template")
+  .option("-v2, --vue2", "vue2 template")
+  .option("-v3, --vue3", "vue3 template")
+  .action((projectName, options) => {
+    console.log(projectName, options);
   });
-program.parse(process.argv);
-// const options = program.opts();
-// console.log(options)
+inquirer
+  .prompt([
+    {
+      type: "list",
+      name: "frameTemplate",
+      message: "请选择框架类型",
+      choices: ["Vue3", "Vue2", "React"],
+    },
+  ])
+  .then((answer) => {
+    console.log(answer);
+  });
+program.version("1.0.0").parse(process.argv);
